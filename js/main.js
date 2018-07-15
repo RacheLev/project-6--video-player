f73ab6a3eb5f35acbe0b4ace89f29eb471550412
Rachel <33595139+RacheLev@users.noreply.github.com>
2018-07-15 14:50:32 +0300

    added red highlight

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+const video = document.getElementById('myVideo');
+const transcript = document.querySelectorAll('span');
+
+
+// Highlighting transcript
+video.addEventListener("timeupdate", () => {
+ for (let i = 0; i < transcript.length; i++) {
+   if(video.currentTime > transcript[i].getAttribute('data-start') && video.currentTime < transcript[i].getAttribute('data-end')){
+     transcript[i].style.color = "red";
+   } else {
+     transcript[i].style.color = "#4D4949";
+   }
+ }
+});
+
+//When the user clicks on any sentence in the transcript the video player jumps to the appropriate time in the video
+ transcript.addEventListener("click", () => {
+  for (let i = 0; i < transcript.length; i++) {
+     if(   ==transcript[i]) {
+        video.currentTime = transcript[i].getAttribute('data-start');
+
+     }
+    }
+   });
